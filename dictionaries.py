dec066390a63196c5c0475fbce75ece0909502fb
mapulaMobs53 <masekwamengmapula5@gmail.com>
2025-06-24 12:45:40 +0200

    numeric operators

--- dictionaries.py
+++ dictionaries.py
@@ -1,4 +1,5 @@
 #a collection of key value pairs
+'''
 my_dictionary = {'apple' : 3, 'banana' : 5, 'orange' :2}
 print(my_dictionary['apple'])
 
@@ -6,4 +7,15 @@ my_dictionary['grape'] = 4
 print(my_dictionary)
 
 my_dictionary['banana'] = 7
-print(my_dictionary)
+print(my_dictionary)'''
+
+#Dictionaries PART 2
+#using numeric data operators
+
+x = 10
+y = 2
+
+print(x+y)
+
+x += 2
+print(x)
