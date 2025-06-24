d2d0fdec6ecad67bc95138070d132e2471b77ba8
mapulaMobs53 <masekwamengmapula5@gmail.com>
2025-06-24 12:40:01 +0200

    Learning basics of dictionaries

---
+++ dictionaries.py
@@ -0,0 +1,9 @@
+#a collection of key value pairs
+my_dictionary = {'apple' : 3, 'banana' : 5, 'orange' :2}
+print(my_dictionary['apple'])
+
+my_dictionary['grape'] = 4
+print(my_dictionary)
+
+my_dictionary['banana'] = 7
+print(my_dictionary)
