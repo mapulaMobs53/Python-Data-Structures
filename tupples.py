da2072b26e7d90b26ed6bce53c56d75e91988a51
mapulaMobs53 <masekwamengmapula5@gmail.com>
2025-06-24 12:26:52 +0200

    Learning about Tuples which are used when data shoudn't be changed

---
+++ tupples.py
@@ -0,0 +1,13 @@
+#similar to lists, but collection of ordered elements, they can'r be changed once ordered
+
+my_tuple = (1,2,3,4,5,)
+print(my_tuple)
+print(my_tuple[0])
+print(my_tuple[2])
+print(my_tuple[-1])
+tuple_1 = (1,2,3)
+tuple_2 = (4,5,6)
+conc_tuple = tuple_1 + tuple_2
+print(conc_tuple)
+rep_tuple = tuple_1 * 3
+print(rep_tuple)
